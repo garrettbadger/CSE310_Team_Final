200f21e584e0584c5df1e3a96024e2edc9f5d9c1
garrettbadger <garrett.badger.id@gmail.com>
2022-03-31 15:57:42 -0600

    update

--- game/src/game.py
+++ game/src/game.py
@@ -103,16 +103,8 @@ class Game(arcade.Window):
 
     def calculateWPM(self):
         #Calculate the words per minute by taking the score or total number of words and then dividing it by the total time it took to type the word and then subtracting any errors
-<<<<<<< HEAD
-        wordsperminute = (self.score / (self.end - self.start))
-        self.avgwpm.append(wordsperminute)
-        # To try and average all the words per minute I store each value in a list and divide it by how many times you have completed a word
-        
-        self.wpm = wordsperminute
-=======
         self.wpm = (self.score / ( (self.end - self.start ) / 60) ) - self.errors
         
->>>>>>> 99c10695f072bbfe78920307f4269c5a4ac624c5
 
         
     
